812c84f8b2ae85bfe78d13aa23e3d04161e8576a
Jakob Evangelista <jakobevangelista@jakobs-macbook-pro.tail1d42c.ts.net>
2024-06-21 15:38:26 -0700

    finished migration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,27 @@
 import { db } from "@/server/neonDb";
 import { userAttributes, users } from "@/server/neonDb/schema";
-import { auth } from "@clerk/nextjs/server";
+import { UserButton } from "@clerk/nextjs";
+import { auth, currentUser } from "@clerk/nextjs/server";
 import { eq } from "drizzle-orm";
 
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const { userId }: { userId: string | null } = await auth();
-  if (userId === null) {
+  const user = await currentUser();
+  const { sessionClaims } = await auth();
+  console.log("SESSION CLAIMS: ", sessionClaims);
+  if (user === null) {
     return redirect("/sign-in");
   }
 
-  const user = await db.query.users.findFirst({
-    where: eq(users.email, session.user?.email!),
-  });
-
   const userAttribute = await db.query.userAttributes.findFirst({
-    where: eq(userAttributes.userId, user?.id!),
+    where: eq(userAttributes.userId, sessionClaims!.userId! as string),
   });
 
   return (
     <>
-      <div>Signed In with Next-Auth</div>
-      <div>{JSON.stringify(session)}</div>
       <div>Special Attribute: {userAttribute?.attribute}</div>
-      <form
-        action={async () => {
-          "use server";
-          await signOut();
-        }}
-      >
-        <button type="submit">Sign Out</button>
-      </form>
+      <UserButton />
     </>
   );
 }
